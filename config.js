8667d124ed4235f944f8750d03d77968eb5fd71b
torgeirlysen <torgeir.lysen@gmail.com>
2026-01-22 13:22:45 +0100

    Revert "disable the zoom thing again"
    This reverts commit 984fa64e6cfa17ca5834c20cb57b94a3b17cf9d8.

--- config.js
+++ config.js
@@ -1,1 +1,30 @@
+window.HOME_CONFIG = {
+  accessToken: 'pk.eyJ1IjoibHJqYWNvYnMiLCJhIjoiY21rM3BoanV0MHBhYjNlcXk0OTZ1bXR5eiJ9.c9gJtz4_0bhKpi50XaKODg',
+  style: 'mapbox://styles/lrjacobs/cmke18zhh00g701sc3nm7fyi1',
+  projection: 'mercator',
+  inset: false,
+  insetOptions: {
+    markerColor: 'orange'
+  },
+  insetPosition: 'bottom-right',
+  initialView: {
+    center: [15, 36],
+    zoom: 4.2,
+    bearing: 0,
+    pitch: 0
+  },
+  sarZonesUrl: 'data/sar-zones-data.geojson',
+  greeceSrrUrl: 'data/greece_srr_combined.geojson',
+  italySrrUrl: 'data/italy_srr_combined.geojson',
+  maltaSrrUrl: 'data/malta_srr.geojson',
+  tunisiaSrrUrl: 'data/tunisia_srr.geojson',
+  libyaSrrUrl: 'data/libya_srr.geojson',
+  egyptSrrUrl: 'data/egypt_srr.geojson',
+  cases: [
+    { title: 'Pylos (Adriana)', url: '/cases/pylos/pylos.html', coordinates: [21.2340, 36.4125] },
+    { title: 'Farmakonisi', url: '/cases/farmakonisi/farmakonisi.html', coordinates: [14.612838, 37.347632] },
+    { title: 'Pserimos', url: '/cases/pserimos/pserimos.html', coordinates: [12.612838, 34.347632] }
+  ]
+};
+
 
